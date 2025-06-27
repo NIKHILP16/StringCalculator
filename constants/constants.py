e19fe664492e53e1df71e5115b5c73e3d3f94cbb
NIKHILP16 <patelnikhil1623@gmail.com>
2025-06-27 19:23:01 +0530

    featCalculator): added key in constants

--- constants/constants.py
+++ constants/constants.py
@@ -4,4 +4,5 @@ Holds application-wide constants such as error messages and limits.
 
 class Errors:
     EMPTY_INPUT = "Invalid input: empty number detected"
+    NEGATIVE_NUMBERS = "negatives not allowed - {}"
 
