225aedd02a3b80ba9318a872eb9781e960e04a57
NIKHILP16 <patelnikhil1623@gmail.com>
2025-06-27 17:13:36 +0530

    test(Calculator): Added test case for new line delimiter and extra test case to test multiple input

--- tests/units/calculator/test_calculator.py
+++ tests/units/calculator/test_calculator.py
@@ -19,3 +19,16 @@ def test_add_method_with_single_number(calculator):
 def test_add_method_with_delimiters_multiple_number(calculator):
     result=calculator.add("1,2")
     assert result==3
+
+def test_add_method_with_delimiters_multiple_number_2(calculator):
+    result=calculator.add("1,2,3,4,5")
+    assert result==15
+
+def test_add_method_with_newline_delimiters(calculator):
+    result=calculator.add("1\n2,3")
+    assert result==6
+
+def test_add_with_invalid_input(calculator):
+    with pytest.raises(ValueError) as exc_info:
+        calculator.add("1,\n")
+    assert "Invalid input" in str(exc_info.value)
