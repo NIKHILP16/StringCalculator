905dc3da960d66584d28b567388271f1b4d9fe30
NIKHILP16 <patelnikhil1623@gmail.com>
2025-06-27 12:14:50 +0530

    test(Calculator): test for add method with empty and single string

--- tests/units/calculator/test_calculator.py
+++ tests/units/calculator/test_calculator.py
@@ -7,3 +7,11 @@ def calculator():
 
 def test_initialize_calculator(calculator):
     assert isinstance(calculator, Calculator)
+
+def test_add_method_with_empty_string(calculator):
+    result=calculator.add("")
+    assert result==0
+
+def test_add_method_with_single_number(calculator):
+    result=calculator.add("1")
+    assert result==1
