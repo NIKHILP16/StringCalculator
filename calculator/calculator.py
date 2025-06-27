aff7cce5d01bcac9f1aae51bd1e2625c08985fd5
NIKHILP16 <patelnikhil1623@gmail.com>
2025-06-27 12:23:12 +0530

    feat(Calculator): added add method in calculator class

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,6 +1,30 @@
+from typing import Optional
 class Calculator:
     """
     A simple calculator class to perform basic arithmetic operations.
 
+    Methods
+    -------
+    add(a, b)
+        Returns the sum of a and b.
+
     """
-    pass
+    def add(self, numbers: str) -> int:
+        """
+        Adds numbers provided in a  string.
+
+        Parameters
+        ----------
+        numbers : str
+            A string containing zero or more integers separated by commas (e.g., "1").
+            An empty string returns 0.
+
+        Returns
+        -------
+        int
+            The sum of the provided numbers.
+        """
+        if not numbers:
+            return 0
+        
+        return int(numbers)
